04921ec8da75e8b569907040db56e2db37241544
Michael Sandino <msandino.projects@gmail.com>
2020-10-02 12:21:43 -0500

    creacion de login  de usuarios - simple

--- js/authentication.js
+++ js/authentication.js
@@ -5,6 +5,19 @@ function registrar(){
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
     });
 }
 
+function acceso (){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+}
