354de28bf6a56c7a52f5b504743ac19ac0d7dcac
Michael Sandino <msandino.projects@gmail.com>
2020-10-06 14:21:32 -0500

    Autenticación de usuario con google y facebook - Test

--- js/authentication.js
+++ js/authentication.js
@@ -1,8 +1,93 @@
-/* Función de registrarse con Google */
-function google(){
-  var provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+/* Función de registrar usuarios con correo */
+const register = document.querySelector("#register");
+register.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var email = document.getElementById('email-register').value;
+    var password = document.getElementById('password-register').value;
 
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    
+  /* Enviar mensaje de verificacion de correo */
+  .then(function(){
+    email_verification()
+  })
+      
+  /* Redireccionamiento si no hay problemas */
+  .then(result => {
+    console.log("registrando...")
+    window.location.replace("../usuario");
+  })
+  /* Mensaje de error final*/
+  .catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    var register_error = document.getElementById('register_error')
+    register_error.innerHTML = 'Correo invalido'
+  });
+});
+
+/* Función de acceso usuarios */
+const login = document.querySelector("#login");
+login.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+
+  /* Redireccionamiento si no hay problemas */
+  .then(result => {
+    console.log("ingresando...")
+    window.location.replace("../usuario");
+  })
+
+  /* Mensaje de error final*/
+  .catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var login_error = document.getElementById('login_error')
+    login_error.innerHTML = 'Correo o contraseña invalido'
+  });
+
+});
+
+/* Función de revisión de usuario logueado */
+function observer (){  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      /* Crear mensaje de bienvenida */
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log("ingresando...")
+      window.location.replace("../usuario");
+    } else {
+      /* Borrar mensajes al cerrar sesión */
+      console.log("Sesión cerrada...")
+    }
+  });
+}
+observer();
+
+/* Función de verificacion de correo */
+function email_verification(){
+  var user = firebase.auth().currentUser;
+  user.sendEmailVerification().then(function() {
+    console.log('Enviando Correo...')
+  }).catch(function(error) {
+    console.log(error)
+  });
+}
+
+/* Función de registrarse con Google */
+function google() {
+  var provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then(function(result) {
     var token = result.credential.accessToken;
     var user = result.user;
@@ -23,7 +108,6 @@ function google(){
 /* Función de registrarse con facebook */
 function facebook() {
   var provider = new firebase.auth.FacebookAuthProvider();
-  provider.addScope('user_birthday');
 
   firebase.auth().signInWithPopup(provider).then(function(result) {
     var token = result.credential.accessToken;
@@ -41,146 +125,3 @@ function facebook() {
     var credential = error.credential;
   });
 }
-
-/* Función de registrar usuarios con correo */
-function registrar(){
-    var email = document.getElementById('email-register').value;
-    var password = document.getElementById('password-register').value;
-
-    /* Creacion de variable para mostrar mensajes de error */
-    var reg_email_error = document.getElementById('reg_email_error')
-    var reg_password_error = document.getElementById('reg_password_error')
-    var reg_email_mess = document.getElementsByClassName("reg_email_mess");
-    var reg_password_mess = document.getElementsByClassName("reg_password_mess");
-
-    /* Mensaje de error en correo*/
-    if (!email) {
-      $('.reg_email_mess').addClass('border-danger');
-      reg_email_error.innerHTML = 'Este campo es obligatorio'
-    }else if(email.indexOf('@') == -1 ){
-      $('.reg_email_mess').addClass('border-danger');
-      reg_email_error.innerHTML = 'El correo debe estar bien formateado'
-    }else{
-      $('.reg_email_mess').removeClass('border-danger');
-      reg_email_error.innerHTML = ''
-    }
-    
-    /* Mensaje de error en contraseña*/
-    if(!password){
-      $('.reg_password_mess').addClass('border-danger');
-      reg_password_error.innerHTML = 'Este campo es obligatorio'
-    }else if (password.length < 6) {
-      $('.reg_password_mess').addClass('border-danger');
-      reg_password_error.innerHTML = 'La contraseña debe tener al menos 6 caracteres' 
-    }else{
-      $('.reg_password_mess').removeClass('border-danger');
-      reg_password_error.innerHTML = ''
-    }
-
-    /* Registro de usuarios */
-    if (email != '' && password != '' && password.length >= 6) {
-      firebase.auth().createUserWithEmailAndPassword(email, password)
-    
-      /* Enviar mensaje de verificacion de correo */
-      .then(function(){
-        verificar()
-      })
-      
-      /* Redireccionamiento si no hay problemas */
-      .then(result => {
-        window.location.replace("../usuario");
-      })
-      
-      /* Mensaje de error final*/
-      .catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        var register_error = document.getElementById('register_error')
-        register_error.innerHTML = 'Correo invalido'
-        
-      });
-    }
-
-}
-
-/* Función de acceso usuarios */
-function acceso(){
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    /* Creacion de variable para mostrar mensajes de error */
-    var email_error = document.getElementById('email_error')
-    var password_error = document.getElementById('password_error')
-    var email_mess = document.getElementsByClassName("email_mess");
-    var password_mess = document.getElementsByClassName("password_mess");
-
-    /* Mensaje de error en correo*/
-    if (!email) {
-      $('.email_mess').addClass('border-danger');
-      email_error.innerHTML = 'Este campo es obligatorio'
-    }else{
-      $('.email_mess').removeClass('border-danger');
-      email_error.innerHTML = ''
-    }
-    
-    /* Mensaje de error en contraseña*/
-    if(!password){
-      $('.password_mess').addClass('border-danger');
-      password_error.innerHTML = 'Este campo es obligatorio'
-    }else{
-      $('.password_mess').removeClass('border-danger');
-      password_error.innerHTML = ''
-    }
-
-    /* Acceso de usuario */
-    if (email != '' && password != '') {
-      firebase.auth().signInWithEmailAndPassword(email, password)
-
-    /* Redireccionamiento si no hay problemas */
-    .then(result => {
-      window.location.replace("../usuario");
-    })
-
-      /* Mensaje de error final*/
-      .catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var login_error = document.getElementById('login_error')
-        login_error.innerHTML = 'Correo o contraseña invalido'
-      });
-    }
-}
-
-/* Función de revisión de usuario logueado */
-function observador (){  
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      /* Crear mensaje de bienvenida */
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      window.location.replace("../usuario");
-    } else {
-      /* Borrar mensajes al cerrar sesión */
-      var contenido = document.getElementById('success-message')
-      contenido.innerHTML = '';
-    }
-  });
-}
-observador();
-
-/* Función de verificacion de correo */
-function verificar(){
-  var user = firebase.auth().currentUser;
-  user.sendEmailVerification().then(function() {
-    console.log('Enviando Correo...')
-  }).catch(function(error) {
-    console.log(error)
-  });
-}
-
--- js/observer.js
+++ js/observer.js
@@ -1,8 +1,8 @@
-function observador (){  
+function observer (){  
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         /* Crear mensaje de bienvenida */
-        aparece(user)
+        message(user)
         var displayName = user.displayName;
         var email = user.email;
         var emailVerified = user.emailVerified;
@@ -21,27 +21,27 @@ function observador (){
       }
     });
   }
-  observador();
+  observer();
   
 /* Mensajes de bienvenida */
-function aparece(user){
+function message(user){
     var user = user;
     var contenido = document.getElementById('success-message')
     var exit = document.getElementById('exit')
     if(user.emailVerified){
       contenido.innerHTML = '<div class="alert alert-success text-center" role="alert">Ha iniciado sesión correctamente,</div>';
-      exit.innerHTML = '<hr class="menu-divider"> <button class="nav-link active btn btn-link" onclick="cerrar()"><img src="../icons/salida.svg" class="pr-2"  alt="icono_salir" height="20px">Cerrar Sesión</button>';
+      exit.innerHTML = '<hr class="menu-divider"> <button class="nav-link active btn btn-link" onclick="exit()"><img src="../icons/salida.svg" class="pr-2"  alt="icono_salir" height="20px">Cerrar Sesión</button>';
     }else{
       contenido.innerHTML = '<div class="alert alert-success text-center" role="alert">Ha iniciado sesión correctamente</div> <div class="alert alert-danger text-center" role="alert">Recuerda: Tu correo no esta verificado</div>';
-      exit.innerHTML = '<hr class="menu-divider"> <button class="nav-link active btn btn-link" onclick="cerrar()"><img src="../icons/salida.svg" class="pr-2"  alt="icono_salir" height="20px">Cerrar Sesión</button>';
+      exit.innerHTML = '<hr class="menu-divider"> <button class="nav-link active btn btn-link" onclick="exit()"><img src="../icons/salida.svg" class="pr-2"  alt="icono_salir" height="20px">Cerrar Sesión</button>';
     }
 }
 
 /* Funcion de cerrar Sesión */
-function cerrar(){
+function exit(){
     firebase.auth().signOut()
     .then(function(){
-      console.log('Saliendo')
+      console.log('Saliendo...')
       window.location.replace("../ingresar");
     })
     .catch(function(error){
