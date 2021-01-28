795b5a3e715553a9e1bd64822bfdfef67864ecca
Michael Sandino <msandino.projects@gmail.com>
2021-01-28 17:09:45 -0500

    Arreglo en barra de carga.

--- usuario/ajax/estudios.js
+++ usuario/ajax/estudios.js
@@ -305,7 +305,7 @@ $("#e_update").submit(function(e){
             processData: false,
         
             beforeSend:function (objeto) {
-              $("#message").html('<div class="progress"><div class="progress-bar mt-2 progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
+              $("#message").html('<div class="progress mt-2"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
             },
             success: function(response)
             {
--- usuario/ajax/seguimientos.js
+++ usuario/ajax/seguimientos.js
@@ -188,7 +188,7 @@ if(url.split('/').reverse()[0] == "cultivo.php"){
         data:parametro,
     
         beforeSend:function (objeto) {
-          $("#message").html('<div class="progress"><div class="progress-bar mt-2 progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
+          $("#message").html('<div class="progress mt-2"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
         },
         success: function(response)
         {
@@ -347,7 +347,7 @@ if(url.split('/').reverse()[0] == "plaga.php"){
         data:parametro,
     
         beforeSend:function (objeto) {
-          $("#message").html('<div class="progress"><div class="progress-bar mt-2 progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
+          $("#message").html('<div class="progress mt-2"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
         },
         success: function(response)
         {
@@ -507,7 +507,7 @@ if(url.split('/').reverse()[0] == "tratamiento.php"){
         data:parametro,
     
         beforeSend:function (objeto) {
-          $("#message").html('<div class="progress"><div class="progress-bar mt-2 progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
+          $("#message").html('<div class="progress mt-2"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>').fadeIn("slow");
         },
         success: function(response)
         {
